e938bc933909cac0dfa354f9f0ba7dbe8b683462
Luís Paulo Domingues <157630664+luis-domingues@users.noreply.github.com>
2024-09-26 11:01:11 -0300

    feat: implement new basic logic for enrolling in bootcamps and with a progression system

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
@@ -20,8 +21,36 @@ public class Main {
         mentoria.setDescricao ("Mentoria para turmas de Java e Kotlin");
         mentoria.setData (LocalDate.now ());
 
-        System.out.println (cursoJava);
-        System.out.println (cursoKotlin);
-        System.out.println (mentoria);
+        Bootcamp bootcampJavaDeveloper = new Bootcamp ();
+        bootcampJavaDeveloper.setNome ("Bootcamp Java Developer");
+        bootcampJavaDeveloper.setDescricao ("Descrição do Bootcamp");
+        bootcampJavaDeveloper.getConteudos ().add (cursoJava);
+        bootcampJavaDeveloper.getConteudos ().add (mentoria);
+
+        Bootcamp bootcampKotlinDeveloper = new Bootcamp ();
+        bootcampKotlinDeveloper.setNome ("Bootcamp Kotlin Developer");
+        bootcampKotlinDeveloper.setDescricao ("Descrição do Bootcamp");
+        bootcampKotlinDeveloper.getConteudos ().add (cursoKotlin);
+        bootcampKotlinDeveloper.getConteudos ().add (mentoria);
+
+        Dev devLuis = new Dev ();
+        devLuis.setNome ("Luís");
+        devLuis.inscreverBootcamp (bootcampJavaDeveloper);
+
+        System.out.println ("Cursos inscritos: " + devLuis.getConteudosInscritos ());
+        devLuis.progredir ();
+        System.out.println ("Cursos inscritos: " + devLuis.getConteudosInscritos ());
+        System.out.println ("Conteúdos concluídos: " + devLuis.getConteudosConcluidos ());
+        System.out.println ("XP: " + devLuis.calcularXp ());
+
+        Dev devJoao = new Dev ();
+        devJoao.setNome ("João");
+        devJoao.inscreverBootcamp (bootcampKotlinDeveloper);
+
+        System.out.println ("Cursos inscritos: " + devJoao.getConteudosInscritos ());
+        devJoao.progredir ();
+        System.out.println ("Cursos inscritos: " + devJoao.getConteudosInscritos ());
+        System.out.println ("Conteúdos concluídos: " + devJoao.getConteudosConcluidos ());
+        System.out.println ("XP: " + devJoao.calcularXp ());
     }
 }
